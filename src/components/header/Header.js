e048e2682d8915eac17b2fc1fb9d3de20491bd2e
unknown <rubayzaf2007@gmail.com>
2021-01-03 10:37:53 +0600

    done much better than before

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,6 +5,11 @@ const Header = () => {
     return (
         <div className="header">
             <img src={Logo} alt="ema-john-logo"/>
+            <nav>
+                <a href="/shop">Shop</a>
+                <a href="/review">Order Review</a>
+                <a href="/manage">Manage Inventory Here</a>
+            </nav>
         </div>
     );
 };
