5afadbe837ced7ddccb4596fd95483341da5f22d
unknown <rubayzaf2007@gmail.com>
2021-01-03 11:05:01 +0600

    might have done not completely btw

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css';
 import Header from './components/header/Header';
+import Shop from './components/shop/Shop';
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Shop></Shop>
     </div>
   );
 }
